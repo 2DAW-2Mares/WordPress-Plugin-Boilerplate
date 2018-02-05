3b50a92b9140953bfb63c4d7c9ce51953b31df4f
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-02-05 12:06:36 +0100

    Información Estándar del Plugin

--- IES2MaresJobs/IES2MaresJobs.php
+++ IES2MaresJobs/IES2MaresJobs.php
@@ -8,17 +8,17 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              http://iesdosmares.com
  * @since             1.0.0
  * @package           IES2MaresJobs
  *
  * @wordpress-plugin
  * Plugin Name:       IES Dos Mares Jobs
- * Plugin URI:        http://example.com/IES2MaresJobs-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin URI:        http://iesdosmares.com/IES2MaresJobs-uri/
+ * Description:       Gestión de ofertas de trabajo para los alumnos del I.E.S. Dos Mares
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            I.E.S. Dos Mares
+ * Author URI:        http://iesdosmares.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       IES2MaresJobs
