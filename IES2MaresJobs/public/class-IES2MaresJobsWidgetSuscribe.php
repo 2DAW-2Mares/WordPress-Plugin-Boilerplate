a450f62c57b32e1aa9f5682c6c42c76454f31aec
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-02-06 13:18:06 +0100

    Separando la vista en los partials de admin y public

--- IES2MaresJobs/public/class-IES2MaresJobsWidgetSuscribe.php
+++ IES2MaresJobs/public/class-IES2MaresJobsWidgetSuscribe.php
@@ -31,44 +31,20 @@ class IES2MaresJobsWidgetSuscribe extends WP_Widget {
 // Creating widget front-end
 
     public function widget( $args, $instance ) {
-        $title = apply_filters( 'widget_title', $instance['title'] );
 
-// before and after widget arguments are defined by themes
-        echo $args['before_widget'];
-        if ( ! empty( $title ) )
-            echo $args['before_title'] . $title . $args['after_title'];
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/IES2MaresJobs-public-display.php';
 
-// This is where you run the code and display the output
-        echo __( 'Suscríbete para recibir nuestras ofertas de trabajo', 'IES2MaresJobsSuscribe_widget_domain' );
-        echo $args['after_widget'];
-?>
-            <form action="http://<?php echo $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>" method="post">
-
-                <p>
-                    <label for="solo-subscribe-email"><?php _e('E-Mail:', 'subscribe-to-comments'); ?>
-                        <input type="text" name="email" id="solo-subscribe-email" size="22" value="" /></label>
-                    <input type="submit" name="submit" value="<?php _e('Subscribe', 'subscribe-to-comments'); ?>" />
-                </p>
-            </form>
-<?php
+        IES2MaresJobsWidgetPublicForm($args, $instance);
     }
 
 
 // Widget Backend
     public function form( $instance ) {
-        if ( isset( $instance[ 'title' ] ) ) {
-            $title = $instance[ 'title' ];
-        }
-        else {
-            $title = __( 'Cabecera', 'IES2MaresJobsSuscribe_widget_domain' );
-        }
-// Widget admin form
-        ?>
-        <p>
-            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-        </p>
-        <?php
+
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/partials/IES2MaresJobs-admin-display.php';
+
+
+        IES2MaresJobsWidgetAdminForm($instance, $this);
     }
 
 // Updating widget replacing old instances with new
--- IES2MaresJobs/public/partials/IES2MaresJobs-public-display.php
+++ IES2MaresJobs/public/partials/IES2MaresJobs-public-display.php
@@ -14,3 +14,27 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<?php
+    function IES2MaresJobsWidgetPublicForm($args, $instance) {
+    $title = apply_filters( 'widget_title', $instance['title'] );
+
+    // before and after widget arguments are defined by themes
+    echo $args['before_widget'];
+    if ( ! empty( $title ) )
+    echo $args['before_title'] . $title . $args['after_title'];
+
+    // This is where you run the code and display the output
+    echo __( 'Suscríbete para recibir nuestras ofertas de trabajo', 'IES2MaresJobsSuscribe_widget_domain' );
+    echo $args['after_widget'];
+    ?>
+    <form action="http://<?php echo $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?>" method="post">
+
+        <p>
+            <label for="solo-subscribe-email"><?php _e('E-Mail:', 'subscribe-to-comments'); ?>
+                <input type="email" name="email" id="solo-subscribe-email" size="22" value="" /></label>
+            <input type="submit" name="submit" value="<?php _e('Subscribe', 'subscribe-to-comments'); ?>" />
+        </p>
+    </form>
+<?php
+}
+?>
